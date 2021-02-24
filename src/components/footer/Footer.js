ae0bdbdbbf84cd488acf9d0dd57acaf573071f9b
unknown <snstotti@gmail.com>
2021-02-24 20:58:27 +0300

    social icons dowland

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,8 +3,10 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div style={{background:'#18363d73',width:'100%',height:'50px'}}>
-          
+        <div style={{background:'#18363d73',width:'100%',padding:'10px'}}>
+            <div>Свяжитесь со мной: </div>
+            <div>Еще проекты</div>
+            <div><a href='https://openweathermap.org/'>API</a></div>
         </div>
     )
 }
